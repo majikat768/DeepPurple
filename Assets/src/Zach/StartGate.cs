7f8521eba0642d9298df780abd1e3dee7baf06f9
Zach Sugano <zachoooo@gmail.com>
2019-01-25 00:52:52 -0800

    Removed previous terrain, created basic starting area with gate

---
+++ Assets/src/Zach/StartGate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGate : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
