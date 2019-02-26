ac172b3547a679fef611e5493ff3c77a3a2a14d6
Robert Goes <RobertRGoes@gmail.com>
2019-02-26 13:50:56 -0800

    Added movement to enemys

--- Assets/src/Robert/BasicEnemy.cs
+++ Assets/src/Robert/BasicEnemy.cs
@@ -7,6 +7,14 @@ public class BasicEnemy : MonoBehaviour, IAttackable
     private GameObject player;
     public int health = 100;
 
+    //Max distance an enemy will begin moving torward player
+    public int moveMax = 100;
+    //Min distance to player and enemy will get
+    public int moveMin = 10;
+
+    //movement speed of enemy
+    public int moveSpeed = 4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +24,15 @@ public class BasicEnemy : MonoBehaviour, IAttackable
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPos = player.transform.position;
+        float distPlayer = Vector3.Distance(transform.position, playerPos);
+
+        if(distPlayer <= moveMax && distPlayer >= moveMin)
+        {
+            transform.LookAt(playerPos);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+
     }
 
     public void takeDamage(DamageSource damageSource)
