31cb8273cb907f9e6377097e93b43683c68e7f7c
Zach Sugano <zachoooo@gmail.com>
2019-02-27 19:00:40 -0800

    Updated files to use LFS and added health to player

--- Assets/src/Oshan/PlayerController.cs
+++ Assets/src/Oshan/PlayerController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IAttackable
 {
     public Rigidbody bullet;
+    private int health = 200;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,5 +27,18 @@ public class PlayerController : MonoBehaviour
         bulletClone.GetComponent<BasicBullet>().launcher = gameObject;
         bulletClone.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
         bulletClone.velocity = transform.forward * 20;
+    }
+
+    public void takeDamage(DamageSource damageSource)
+    {
+        if (!Registry.GetOrDefault<bool>("bcmode", false))
+        {
+            health -= damageSource.baseDamage;
+        }
+    }
+
+    public int GetHealth()
+    {
+        return health;
     }
 }
