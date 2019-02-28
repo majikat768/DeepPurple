918cea2ef1c93d1cfa1227f8656ef36f1aaa3d45
gabrielhasen <47019501+gabrielhasen@users.noreply.github.com>
2019-02-27 18:01:47 -0800

    Various UI Changes

---
+++ Assets/src/Gabriel/Main_Menu_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Script : MonoBehaviour 
+{
+	public void PlayGame()
+	{
+		if(SceneManager.GetActiveScene().buildIndex == 0)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		else
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		}
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
