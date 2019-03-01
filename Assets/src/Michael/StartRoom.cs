e00793fe4cebe8a8dcf8f526d1bc02bdc91ec6e0
Zach Sugano <zachoooo@gmail.com>
2019-02-28 20:02:08 -0800

    Created a few level generation schemas

--- Assets/src/Michael/StartRoom.cs
+++ Assets/src/Michael/StartRoom.cs
@@ -9,11 +9,13 @@ public class StartRoom : Room
         // Here, player represented by purple block
         // 
         Vector3 SpawnPoint = new Vector3(size.x/2,0.5f,1.0f);
+        /** Player object spawned by default
         GameObject Player = GameObject.Instantiate(PlayerBall, SpawnPoint, Quaternion.identity, room.transform);
         Player.GetComponent<Renderer>().material.SetColor("_Color", Color.magenta);
         Player.transform.parent = room.transform;
         Player.tag = "Player";
         Debug.Log("start");
+        */
     }
 
 }
--- Assets/src/Zach/LevelGenerator.cs
+++ Assets/src/Zach/LevelGenerator.cs
@@ -4,24 +4,138 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject[] availableRooms;
+    public enum Generator
+    {
+        LINEAR, RANDOM, SQUARE
+    }
 
     public void Start()
     {
-        Room r;
-        for (int i = 0; i < 5; i++)
+        generateLevel(Generator.RANDOM);
+    }
+
+    public void generateLevel(Generator generator)
+    {
+        switch (generator)
         {
-            for (int j = 0; j < 5; j++)
-            {
-                r = RoomGenerator.Get(new Vector3(16 * i, 0, 16 * j), RoomGenerator.RoomType.Combat);
-                r.Generate();
-            }
+            case Generator.SQUARE:
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Room r;
+                        if (i == j && j == 0)
+                        {
+                            r = RoomGenerator.Get(new Vector3(16 * i, 0, 16 * j), RoomGenerator.RoomType.Start);
+                        }
+                        else if (i == j && j == 4)
+                        {
+                            r = RoomGenerator.Get(new Vector3(16 * i, 0, 16 * j), RoomGenerator.RoomType.Boss);
+                        }
+                        else
+                        {
+
+                            r = RoomGenerator.Get(new Vector3(16 * i, 0, 16 * j), RandomRoomType());
+                        }
+                        r.Generate();
+                    }
+                }
+                break;
+            case Generator.LINEAR:
+                for (int i = 0; i < 25; i++)
+                {
+                    Room r;
+                    if (i == 0)
+                    {
+                        r = RoomGenerator.Get(new Vector3(16 * i, 0, 0), RoomGenerator.RoomType.Start);
+                    }
+                    else if (i == 24)
+                    {
+                        r = RoomGenerator.Get(new Vector3(16 * i, 0, 0), RoomGenerator.RoomType.Boss);
+                    }
+                    else
+                    {
+                        r = RoomGenerator.Get(new Vector3(16 * i, 0, 0), RandomRoomType());
+                    }
+                    r.Generate();
+                }
+                break;
+            case Generator.RANDOM:
+                List<Vector2Int> offsets = new List<Vector2Int>();
+                offsets.Add(new Vector2Int(0, 0)); // Spawn room
+
+                int numLoops = Random.Range(25, 50);
+                for (int i = 0; i < numLoops; i++)
+                {
+                    bool foundEmpty = false;
+                    while (!foundEmpty)
+                    {
+                        Vector2Int vector2Int = offsets[Random.Range(0, offsets.Count)]; // Pick random offset from list
+                        // Pick a random direction
+                        int dir = Random.Range(0, 4); // 0 North, 1 East, 2 South, 3 West
+                        Vector2Int newLoc = new Vector2Int(vector2Int.x, vector2Int.y);
+                        switch (dir)
+                        {
+                            case 0:
+                                newLoc.y += 1;
+                                break;
+                            case 1:
+                                newLoc.x += 1;
+                                break;
+                            case 2:
+                                newLoc.y -= 1;
+                                break;
+                            case 3:
+                                newLoc.x -= 1;
+                                break;
+                        }
+                        if (!offsets.Contains(newLoc))
+                        {
+                            offsets.Add(newLoc);
+                            foundEmpty = true;
+                        }
+                    }
+                }
+                for (int i = 0; i < offsets.Count; i++)
+                {
+                    Vector2Int vector2Int = offsets[i];
+                    Room r;
+                    if (vector2Int.x == 0 && vector2Int.y == 0)
+                    {
+                        r = RoomGenerator.Get(new Vector3(0, 0, 0), RoomGenerator.RoomType.Start);
+                    }
+                    else if (i == offsets.Count - 1)
+                    {
+                        r = RoomGenerator.Get(new Vector3(16 * vector2Int.x, 0, 16 * vector2Int.y), RoomGenerator.RoomType.Boss);
+                    }
+                    else
+                    {
+                        r = RoomGenerator.Get(new Vector3(16 * vector2Int.x, 0, 16 * vector2Int.y), RandomRoomType());
+                    }
+                    r.Generate();
+                }
+                break;
         }
         RoomGenerator.BuildDoors();
     }
 
-    public void generateLevel()
+    public RoomGenerator.RoomType RandomRoomType()
     {
-
+        RoomGenerator.RoomType rt;
+        float rand = Random.Range(0f, 1.0f);
+        if (rand < 0.6)
+        {
+            rt = RoomGenerator.RoomType.Combat;
+        }
+        else if (rand < 0.9)
+        {
+            rt = RoomGenerator.RoomType.Puzzle;
+        }
+        else
+        {
+            rt = RoomGenerator.RoomType.Treasure;
+        }
+        return rt;
     }
+
 }
