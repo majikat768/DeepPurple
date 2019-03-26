2b6189ae6ded4b3026f347a9dcc3a18c1009074d
Zach Sugano <zachoooo@gmail.com>
2019-03-25 23:05:35 -0700

    Created unit test for Registry

---
+++ Assets/tst/Zach/Editor/RegistryTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class RegistryTest {
+
+	[Test]
+	public void GetOrDefaultTest() {
+        Assert.AreEqual(Registry.GetOrDefault<int>("test", -1), -1);
+        Assert.AreNotEqual(Registry.GetOrDefault<int>("test", 0), 1);
+        Registry.registry["test"] = 5;
+        Assert.AreEqual(Registry.GetOrDefault<int>("test", 0), 5);
+    }
+}
