ff0930c14868f07392e1b2d658e2cd47274e44f4
gabrielhasen <gabrielhasenoehrl@gmail.com>
2019-03-26 20:41:32 -0700

    Added debug log to show pass/fail

---
+++ Assets/src/Gabriel/TESTING_PASSFAIL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTING_PASSFAIL : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		StartCoroutine(TestingCo());
+	}
+
+	public IEnumerator TestingCo()
+	{
+	 	yield return new WaitForSeconds(1);
+		 if((1.0f / Time.deltaTime) < 5)
+		 {
+			 Debug.Log("Fail");
+		 }
+		 else
+		 {
+			 Debug.Log("Pass");
+		 }
+	}
+}
