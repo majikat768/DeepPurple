49a306ee7aa79c282e8f850960851ea87b47b0de
Oshan Karki <kark6037@vandals.uidaho.edu>
2019-04-01 21:55:22 -0700

    Added script for health system, not fully operational

---
+++ Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/Generic/CheckHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckHealth : MonoBehaviour {
+
+	private void Start(){
+		HealthSystem healthSystem = new HealthSystem(100);
+
+		Debug.Log("Health: "+healthSystem.GetHealth());
+	}
+}
---
+++ Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/Generic/HealthSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour 
+{
+
+	private int health;
+
+	public HealthSystem(int health)
+	{
+		this.health = health;
+	}
+
+	public int GetHealth()
+	{
+		return health;
+	}
+}
