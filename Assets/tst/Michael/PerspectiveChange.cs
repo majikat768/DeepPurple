a47aec9386a2ccec26b34ac4723f7a24d8da303a
Michael <atki7828@vandals.uidaho.edu>
2019-04-04 11:41:37 -0700

    testing toggling camera view

---
+++ Assets/tst/Michael/PerspectiveChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// all this does is moves the camera's following distance to 
+// in front of the player character, making it appear as first person.
+//
+// some other changes that could be made (in Update() when toggling)
+// could be changing the camera's field of view, or height, to make it look better
+
+public class PerspectiveChange : MonoBehaviour {
+
+    GameObject player;
+    Camera cam;
+    KeyCode toggle = KeyCode.Z;
+    float thirdPersonDistance;
+    bool firstPerson = false;
+
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        cam = Camera.main;
+        thirdPersonDistance = cam.GetComponent<vThirdPersonCamera>().defaultDistance;
+		
+	}
+	
+    // When player hits toggle button (Z), 
+    // change the vThirdPersonCamera component's follow distance to -1.
+    // this puts the camera slightly in front of the player character. 
+	void Update () {
+        if(Input.GetKeyDown(toggle)) {
+            if(!firstPerson) {
+                cam.GetComponent<vThirdPersonCamera>().defaultDistance = -0.5f;
+                firstPerson = true;
+            }
+            else {
+                cam.GetComponent<vThirdPersonCamera>().defaultDistance = thirdPersonDistance;
+                firstPerson = false;
+            }
+        }
+
+		
+	}
+}
