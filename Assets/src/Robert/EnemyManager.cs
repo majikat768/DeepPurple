013e11934d7ba423afdecfb429104a799e46b819
Robert Goes <RobertRGoes@gmail.com>
2019-04-06 18:46:17 -0700

    Updated Soldier prefab

--- Assets/src/Robert/EnemyManager.cs
+++ Assets/src/Robert/EnemyManager.cs
@@ -6,11 +6,14 @@ public class EnemyManager : MonoBehaviour {
     //refrence to the player
     private GameObject player;
     private GameObject enemy;
+    private GameObject room;
+
     public string enemy_t;
     //game objects representing the spawn points.
     //TODO: just pass a ist of vector 3d's
     public List<Vector3> SpawnPoints;
     // Use this for initialization
+
     void Start () {
        //if( enemy_t == "basic")
        if(SpawnPoints == null) {
@@ -31,6 +34,7 @@ public class EnemyManager : MonoBehaviour {
         foreach (Vector3 spawnPoint in SpawnPoints)
         {
             GameObject e = Instantiate(enemy, spawnPoint, Quaternion.identity);
+            e.transform.parent = this.transform;
             //e.AddComponent<BasicEnemy>();
         }
     }
