0f812ca41225f1c949e5e88522dd723a3dc848da
kjhild <hild2801@vandals.uidaho.edu>
2019-04-09 16:13:05 -0700

    Item Database/System Start

---
+++ Assets/src/Kyle/ItemDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemDatabase {
+	public static GameObject[] AllItems = Resources.LoadAll<GameObject>("Kyle/Items");
+	public static GameObject InvulnPotRef = Resources.Load<GameObject>("Kyle/Items/Invulnerability");
+
+
+
+	public static GameObject RandomItemGrabber()
+	{
+		GameObject i = AllItems[Random.Range(0,AllItems.Length)];
+			return i;
+	}
+}
+
---
+++ Assets/src/Kyle/TestItemSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItemSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GameObject.Instantiate (ItemDatabase.InvulnPotRef);
+	}
+}
